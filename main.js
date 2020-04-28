ffec563ca564304cab71b3192d17fca3988ad2d8
unknown <stoichevrado@gmail.com>
2020-04-28 17:03:54 +0300

    fetch

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 function fetchIssues(){
-    
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    var issueList = document.getElementById('issuesList');
+
+    issueList.innerHTML = '';
+
+    for(var i = 0; i < issues.length; i++){
+        var id = issues[i].id;
+        var desc = issues[i].description;
+        var severity = issues[i].severity;
+        var assignedTo = issues[i].assignedTo;
+        var status = issues[i].status;
+
+        issuesList.innerHTML += '<div class="well">' +
+                                '<h6>Issue ID: ' + id + '</h6>'+
+                                '<p><span class="label label-info">' + status + '</span></p>' +
+                                '<h3>' + desc + '</h3>' + 
+                                '<p><span class="glyphicon glyphicon-time"></span>' + severity + '</p>' +
+                                '<p><span class="glyphicon glyphicon-time"></span>' + assignedTo + '</p>' + 
+                                '<a href="#" onclick="setStatusClosed(\''+id+'\')"class="btn btn-warning">Close</a>'+
+                                '<a href="#" onclick="deleteIssue(\''+id+'\')"class="btn btn-danger">Delete</a>' +
+                                '<div>'
+    }
 }
